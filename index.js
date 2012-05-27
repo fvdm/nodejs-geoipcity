6dbb0ace2d668ade61d47593269469346193736a
Franklin <fr@nkl.in>
2012-05-27 16:55:14 +0200

    Rewritten module for better structure, not compatible with previous releases.
    The license key is moved from lookup() to settings.license var

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
+/*
+	This module is COPYLEFT meaning you can do anything you want,
+	except copyrighting it.
+	
+	For future reference, please include the source URL:
+	http://github.com/fvdm/nodejs-geoipcity
+*/
+
 var http = require('http');
+var EventEmitter = require('events').EventEmitter;
 
-var lookup = function( license, ip, callback ) {
-	http.get({ host: 'geoip3.maxmind.com', port: 80, path: '/f?l='+ license +'&i='+ ip }, function( result ) {
-		result.setEncoding('utf8');
-		result.on('data', function( data ) {
-			var data = ip +','+ data;
-			callback( parseResult( data ) );
-		});
-	});
+var app = new EventEmitter();
+
+// settings
+app.settings = {
+	apihost:	'geoip3.maxmind.com',
+	apiport:	80,
+	license:	''
 }
-exports.lookup = lookup;
 
-var parseResult = function( str ) {
-	var result = [];
+// do lookup
+app.lookup = function( ip, callback ) {
+	http.get(
+		{
+			host: app.settings.apihost,
+			port: app.settings.apiport,
+			path: '/f?l='+ app.settings.license +'&i='+ ip
+		},
+		function( result ) {
+			result.setEncoding('utf8');
+			result.on('data', function( data ) {
+				var data = ip +','+ data;
+				callback( app.parseResult( data ) );
+			});
+		}
+	);
+}
+
+// parse csv data to object
+app.parseResult = function( str ) {
+	var result = {};
 	var head = ['target', 'countryCode', 'regionCode', 'city', 'postalCode', 'latitude', 'longitude', 'metroCode', 'areaCode', 'isp', 'org', 'extra'];
 	var str = str.split(',');
 	for( i=0; i<str.length; i++ ) {
@@ -23,4 +49,6 @@ var parseResult = function( str ) {
 	}
 	return result;
 }
-exports.parseResult = parseResult;
+
+// ready
+module.exports = app;
