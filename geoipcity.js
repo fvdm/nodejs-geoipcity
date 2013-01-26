61bd068217fce10c44c806b8256110cc0ef32674
Franklin van de Meent <fr@nkl.in>
2013-01-27 00:54:14 +0100

    Replaced copyleft license with Unlicense, closes #4

--- geoipcity.js
+++ geoipcity.js
@@ -1,9 +1,34 @@
 /*
-	This module is COPYLEFT meaning you can do anything you want,
-	except copyrighting it.
-	
-	For future reference, please include the source URL:
-	http://github.com/fvdm/nodejs-geoipcity
+Name:         geoipcity
+Description:  Lookup details for an IP using the Maxmind GeoIP City webservice.
+Source:       https://github.com/fvdm/nodejs-geoipcity
+Feedback:     https://github.com/fvdm/nodejs-geoipcity/issues
+License:      Unlicense / Public Domain
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
 */
 
 var http = require('http');
