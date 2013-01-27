08f3a411aa55f6814f3fa6824396f939001212bb
Franklin van de Meent <fr@nkl.in>
2013-01-27 18:05:12 +0100

    Removed semicolons

--- geoipcity.js
+++ geoipcity.js
@@ -113,17 +113,17 @@ app.lookup = function( ip, callback ) {
 
 // parse csv data to object
 app.parseResult = function( str ) {
-	var result = {};
-	var head = ['target', 'countryCode', 'regionCode', 'city', 'postalCode', 'latitude', 'longitude', 'metroCode', 'areaCode', 'isp', 'org', 'extra'];
-	var str = str.split(',');
+	var result = {}
+	var head = ['target', 'countryCode', 'regionCode', 'city', 'postalCode', 'latitude', 'longitude', 'metroCode', 'areaCode', 'isp', 'org', 'extra']
+	var str = str.split(',')
 	for( i=0; i<str.length; i++ ) {
 		if( str[i].substr(0,1) == '"' && str[i].substr(-1,1) == '"' ) {
-			str[i] = str[i].substr( 1, str[i].length -2 );
+			str[i] = str[i].substr( 1, str[i].length -2 )
 		}
-		result[ head[i] ] = str[i];
+		result[ head[i] ] = str[i]
 	}
-	return result;
+	return result
 }
 
 // ready
-module.exports = app;
+module.exports = app
